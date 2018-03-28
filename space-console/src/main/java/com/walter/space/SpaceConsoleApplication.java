dff202c90ca2d13ebbc41d940c34c3feeb5e4c04
walterwu <walterwu@tcl.com>
2018-03-28 17:18:30 +0800

    Initial commit

---
+++ space-console/src/main/java/com/walter/space/SpaceConsoleApplication.java
@@ -0,0 +1,12 @@
+package com.walter.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceConsoleApplication.class, args);
+	}
+}
---
+++ space-console/src/test/java/com/walter/space/SpaceConsoleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.walter.space;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpaceConsoleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
