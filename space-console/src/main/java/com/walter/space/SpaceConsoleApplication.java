c3e3c54db3a526e73c5379ae14a96ab5b27fe51c
walter52 <walter52@sohu.com>
2018-03-29 17:30:55 +0800

    添加logback日志模块

--- space-console/src/main/java/com/walter/space/SpaceConsoleApplication.java
+++ space-console/src/main/java/com/walter/space/SpaceConsoleApplication.java
@@ -1,13 +1,18 @@
 package com.walter.space;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpaceConsoleApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(SpaceConsoleApplication.class);
 
 	public static void main(String[] args) {
+
+		LOG.info("**********spring boot starting**********");
 		SpringApplication.run(SpaceConsoleApplication.class, args);
-		System.out.println("spring boot 启动");
+		LOG.info("**********spring boot success**********");
 	}
 }
