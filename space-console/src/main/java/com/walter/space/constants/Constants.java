ecc0a7ee8cc66bc7523f7b2dc69e36734ec58431
walter52 <walter52@sohu.com>
2018-04-01 16:56:19 +0800

    完成request filter过滤方法

---
+++ space-console/src/main/java/com/walter/space/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.walter.space.constants;
+
+/**
+ * @author walterwu
+ * @date 2018/04/01
+ */
+public class Constants {
+
+  /**
+   * 保存已登录管理员的会话Key
+   */
+  public static final String LOGIN_ADMIN_SESSION_KEY = "loginAdmin";
+
+}
