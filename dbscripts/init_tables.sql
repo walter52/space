a79f8e48a2970eee02efc443a871f827143ad6eb
walter52 <walter52@sohu.com>
2020-01-27 23:16:59 +0800

    修改配置文件

--- dbscripts/init_tables.sql
+++ dbscripts/init_tables.sql
@@ -4,6 +4,8 @@ CREATE TABLE `space_console_admin` (
   `email` varchar(128) NOT NULL COMMENT '电子邮箱',
   `password` varchar(32) NOT NULL COMMENT '登录密码',
   `name` varchar(128) NOT NULL COMMENT '管理员名称',
+  `role_code` varchar(128) NOT NULL COMMENT '角色编码',
+  `status` varchar(128) NOT NULL COMMENT '管理员状态',
   `remark` varchar(128) DEFAULT NULL COMMENT '备注信息',
   `created_by` varchar(128) NOT NULL COMMENT '创建人',
   `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
