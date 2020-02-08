932e7d1109defe9e09f45230180f17afb5253d28
walter52 <walter52@sohu.com>
2020-02-09 01:50:09 +0800

    加入eureka工程

---
+++ space-eureka/src/test/java/com/walter/space/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walter.space.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
