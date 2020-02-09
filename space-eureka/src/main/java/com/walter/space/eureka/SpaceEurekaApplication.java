2811142450a02a22522d8bd1462215e3a5711f3d
walter52 <walter52@sohu.com>
2020-02-09 11:42:44 +0800

    添加eureka配置

--- space-eureka/src/main/java/com/walter/space/eureka/SpaceEurekaApplication.java
+++ space-eureka/src/main/java/com/walter/space/eureka/SpaceEurekaApplication.java
@@ -2,12 +2,14 @@ package com.walter.space.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class EurekaApplication {
+@EnableEurekaServer
+public class SpaceEurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(SpaceEurekaApplication.class, args);
 	}
 
 }
