1e625dcbb7376c6ef7cb18a7b1ae7af9a8aab2a2
walter52 <walter52@sohu.com>
2020-02-09 23:09:37 +0800

    add usercenter

---
+++ space-usercenter/src/main/java/com/walter/space/usercenter/UsercenterApplication.java
@@ -0,0 +1,15 @@
+package com.walter.space.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UsercenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsercenterApplication.class, args);
+    }
+
+}
