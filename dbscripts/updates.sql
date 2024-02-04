ed8e1f6d35d7b15e213f8f76ee77119ea18fc767
walter52 <walter52@sohu.com>
2024-02-05 02:12:15 +0800

    更新sql

--- dbscripts/updates.sql
+++ dbscripts/updates.sql
@@ -1,3 +1,5 @@
 ### 1.系统管理员表
 INSERT INTO space_console_admin(email,PASSWORD,NAME,role_code,STATUS,created_by,created_on,last_changed_by,last_changed_on) 
-VALUES("walter@sohu.com","123456","walter","1","AVAILABLE","walter@sohu.com",NOW(),"walter@sohu.com",NOW());
+VALUES("walter@sohu.com","c89e5206cee97793db5e27a632dd66b7","walter","1","AVAILABLE","walter@sohu.com",NOW(),"walter@sohu.com",NOW());
+
+
